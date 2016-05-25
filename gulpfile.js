a9c7c10b75695e580b073b7cd0db03c2b8971add
kendavis416 <ken.davis@d2solutionsgroup.com>
2016-05-24 20:39:49 -0400

    Gulp File completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,60 @@
-"use strict;"
+"use strict";
 
 var gulp = require('gulp');
 var connect = require('gulp-connect');
-var open = require('gulp-open');
+var cleanCss = require('gulp-clean-css');
 var concat = require('gulp-concat');
 var less = require('gulp-less');
-var cleanCss = require('gulp-clean-css');
+var open = require('gulp-open');
 
-var confi = {
-	port: 5000,
-	baseUrl: 'http://localhost',
+var config = {
+	rootUrl: "http://localhost",
+	port: 42016,
 	paths: {
-		less: './src/less/*less',
-		html: './src/*.html',
-		js: './src/scripts/*.js',
-		dist: './dist'
+		html: "./src/*.html",
+		js: "./src/scripts/*.js",
+		less: "./src/less/*.less",
+		dist: "./dist"
 	}
 };
 
-gulp.task('open', ['connect'], function () {
-	gulp.src('dist/index.html')
-		.pipe(open({ uri: config.baseUrl + ':' + config.port + '/'}));
-});
-
-gulp.task('js', function(){
-	return gulp.src(config.paths.js)
-		.pipe(gulp.dest(config.paths.dist + '/scripts'))
-		.pipe(connect.reload());
-});
-
-gulp.task('html', function(){
+gulp.task('html', function() {
 	return gulp.src(config.paths.html)
 		.pipe(gulp.dest(config.paths.dist))
 		.pipe(connect.reload());
 });
 
-gulp.task('less', function(){
+gulp.task('js', function() {
+	return gulp.src(config.paths.js)
+		.pipe(gulp.dest(config.paths.dist + '/scripts'))
+		.pipe(connect.reload());
+});
+
+gulp.task('less', function() {
 	return gulp.src(config.paths.less)
 		.pipe(less())
-		.pipe(concat('bundle.css'))
-		.pipe(cleanCss())
 		.pipe(gulp.dest(config.paths.dist + '/css'))
 		.pipe(connect.reload());
 });
 
-gulp.task('connect', function(){
+gulp.task('open', ['connect'], function() {
+	gulp.src('dist/index.html')
+		.pipe(open({ uri: config.rootUrl + ':' + config.port + '/' }));
+});
+
+gulp.task('connect', function() {
 	connect.server({
 		root: ['dist'],
 		port: config.port,
-		base: config.baseUrl,
+		base: config.rootUrl,
 		livereload: true
 	})
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
 	gulp.watch(config.paths.html, ['html']);
 	gulp.watch(config.paths.js, ['js']);
 	gulp.watch(config.paths.less, ['less']);
 });
 
-gulp.task('default', ['less', 'js', 'html', 'open', 'watch']);
+gulp.task('default',['html','js','less','open','watch']);
---
+++ gulpfile1.js
@@ -0,0 +1,62 @@
+"use strict";
+
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+var cleanCss = require('gulp-clean-css');
+
+var confi = {
+	port: 5000,
+	baseUrl: 'http://localhost',
+	paths: {
+		less: './src/less/*less',
+		html: './src/*.html',
+		js: './src/scripts/*.js',
+		dist: './dist'
+	}
+};
+
+gulp.task('open', ['connect'], function () {
+	gulp.src('dist/index.html')
+		.pipe(open({ uri: config.baseUrl + ':' + config.port + '/'}));
+});
+
+gulp.task('js', function(){
+	return gulp.src(config.paths.js)
+		.pipe(gulp.dest(config.paths.dist + '/scripts'))
+		.pipe(connect.reload());
+});
+
+gulp.task('html', function(){
+	return gulp.src(config.paths.html)
+		.pipe(gulp.dest(config.paths.dist))
+		.pipe(connect.reload());
+});
+
+gulp.task('less', function(){
+	return gulp.src(config.paths.less)
+		.pipe(less())
+		.pipe(concat('bundle.css'))
+		.pipe(cleanCss())
+		.pipe(gulp.dest(config.paths.dist + '/css'))
+		.pipe(connect.reload());
+});
+
+gulp.task('connect', function(){
+	connect.server({
+		root: ['dist'],
+		port: config.port,
+		base: config.baseUrl,
+		livereload: true
+	})
+});
+
+gulp.task('watch', function () {
+	gulp.watch(config.paths.html, ['html']);
+	gulp.watch(config.paths.js, ['js']);
+	gulp.watch(config.paths.less, ['less']);
+});
+
+gulp.task('default', ['less', 'js', 'html', 'open', 'watch']);
